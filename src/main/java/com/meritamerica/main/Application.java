99e16db3abb57bda3d2c4c21915988fffef4bcac
HuyCam <camhuy165@gmail.com>
2020-05-08 17:23:25 -0500

    Add database link and enable repository

--- src/main/java/com/meritamerica/main/Application.java
+++ src/main/java/com/meritamerica/main/Application.java
@@ -2,7 +2,9 @@ package com.meritamerica.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.meritamerica.main.repositories")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
